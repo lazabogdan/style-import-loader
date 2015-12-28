0b77bdfe94da5063e03068fbc18b47ac18e1226a
Laza Bogdan <laza.bogdan@macro-net.ro>
2015-12-28 14:25:46 +0200

    update test/webpack.config.js

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         path: path.join(__dirname, '..', 'build'),
         filename: 'test.js',
     },
-    lessImportLoader: {
+    styleImportLoader: {
         base: path.join(process.cwd(), 'test/less/base')
     },
     anotherConfig: {
